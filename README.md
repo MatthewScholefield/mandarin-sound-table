15505914f3b12e59666b122c7bba69800661e4df
Matthew Scholefield <matthew331199@gmail.com>
2019-01-21 15:59:00 -0600

    Updates

--- README.md
+++
@@ -1,9 +0,0 @@
-# Mandarin Sound Table
-
-This is a simple sound table built using Material UI that displays all Mandarin Chinese syllables in a table.
-It fetches the sounds from the [Tone Perfect dataset](https://tone.lib.msu.edu/).
-
-```
-Catherine Ryu, Mandarin Tone Perception & Production Team,
-and Michigan State University Libraries Digital Collections Team (2017). Tone Perfect (https://tone.lib.msu.edu/).
-```
---
+++ precache-manifest.9ce31011bf6af17cf405693b1e9bd271.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ad7e4face0a73e9fae0",
+    "url": "/mandarin-sound-table/static/js/runtime~main.7ad7e4fa.js"
+  },
+  {
+    "revision": "2bef0032e33c9622ec02",
+    "url": "/mandarin-sound-table/static/js/main.2bef0032.chunk.js"
+  },
+  {
+    "revision": "f1b1c63e51b0f2f6d95d",
+    "url": "/mandarin-sound-table/static/js/1.f1b1c63e.chunk.js"
+  },
+  {
+    "revision": "2bef0032e33c9622ec02",
+    "url": "/mandarin-sound-table/static/css/main.5d345515.chunk.css"
+  },
+  {
+    "revision": "bb9cd3e2c9bae022332fea5e2f73df49",
+    "url": "/mandarin-sound-table/index.html"
+  }
+];
